b06fd820ce4bb39dab19fd3854fee89026adf0f4
Ekamjot Singh Bhullar <123894878+EkamBhullar@users.noreply.github.com>
2023-12-12 02:14:23 +0530

    Add files via upload

---
+++ QuizData.js
@@ -0,0 +1,27 @@
+export const QuizData = [
+    {
+        question: "Which planet is known as the Red Planet?",
+        options: ["Mars", "Venus", "Jupiter", "Saturn"],
+        answer: 1
+    },
+    {
+        question: "What is the capital city of Japan?",
+        options: ["Seoul", "Beijing", "Tokyo", "Bangkok"],
+        answer: 3
+    },
+    {
+        question: "Who wrote the play 'Romeo and Juliet'?",
+        options: ["Charles Dickens", "Jane Austen", "William Shakespeare", "Mark Twain"],
+        answer: 2
+    },
+    {
+        question: "Which ocean is the largest?",
+        options: ["Indian Ocean", "Atlantic Ocean", "Arctic Ocean", "Pacific Ocean"],
+        answer: 4
+    },
+    {
+        question: "In which year did the Titanic sink?",
+        options: ["1910", "1912", "1915", "1918"],
+        answer: 2
+    },
+];
